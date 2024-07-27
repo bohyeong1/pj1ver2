41926919fe4ce0a1637a9a07fb803f55c5246f32
서보형 <tjqhgud9104@naver.com>
2024-07-27 15:41:53 +0900

    배포

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                 loader: 'file-loader',
                 options: {
                   name: '[name].[hash].[ext]',
-                  outputPath: 'bundleImg', 
+                  outputPath: '../bundleImg', 
                   publicPath: '/bundleImg',
                 },
               },
